0f277c6a47b413e022459bc58ff936c7e9701b1e
dv.suhotski <dv.sukhotski@lab.vebtech.by>
2021-05-06 12:53:38 +0300

    lesson 2 task trim symbol

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let pos = 0;
+  let result = '';
+  
+  if (typeof size === 'undefined') {
+    return string;
+  }
+  
+  for (let i = 0; i < string.length; i++) {
+    if (i !== string.length - 1 && string[i] !== string[i + 1]) {
+      if ((i + 1) - pos <= size) {
+        result = result + string.slice(pos, i + 1);
+        pos = i + 1;
+      } else {
+        result = result + string.slice(pos, pos + size);
+        pos = i + 1;
+      }
+    }
+    
+    if (i === string.length - 1) {
+      result = result + string.slice(pos, pos + size )
+    }
+  }
+  
+  return result;
 }
