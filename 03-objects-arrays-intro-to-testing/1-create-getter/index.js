1b403676f197c30c7ec7f07ac673c63b44653086
dv.suhotski <dv.sukhotski@lab.vebtech.by>
2021-05-06 18:04:35 +0300

    lesson 2 task create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  let arrayPath = path.split('.')
+  
+  return function(obj) {
+    
+    function recurs(obj, key) {
+      if (typeof obj[key] === 'undefined') return undefined;
+      if (typeof obj[key] === 'string' || typeof obj[key] === 'number') {
+        return obj[key]
+      } else {
+        arrayPath.shift()
+        return recurs(obj[key], arrayPath[0])
+      }
+      
+    }
+    return recurs(obj, arrayPath[0])
+  } 
 }
