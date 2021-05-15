3a71e17b48eb745b1c4a5081204fe2d02e3322d9
dv.suhotski <dv.sukhotski@lab.vebtech.by>
2021-05-15 19:53:34 +0300

    lesson 5 task 1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
-export default class NotificationMessage {
+let timerID = null;
 
+export default class NotificationMessage {
+  constructor(text = "", {duration = 1000, type = "success"} = {}) {
+    this.text = text;
+    this.duration = duration;
+    this.type = type;
+    this.element = null;
+    this.render();
+  }
+
+  get templateAlert() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.text}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const createElem = document.createElement('div');
+    createElem.innerHTML = this.templateAlert;
+    this.element = createElem.firstElementChild;
+  }
+
+  show(wrapper) {
+    if (wrapper) {
+      wrapper.innerHTML = this.element;
+      this.element = wrapper;
+    }
+    if (timerID) {
+      clearTimeout(timerID);
+      document.querySelector('.notification').remove();
+    } 
+    document.body.append(this.element);
+    this.remove();
+  }
+
+  remove() {
+    timerID = setTimeout(() => {
+      document.querySelector('.notification').remove();
+      this.element = null;
+      timerID = null;
+    }, this.duration);
+  }
+
+  destroy() {
+    this.element = null;
+  }
 }
